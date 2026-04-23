6d1af3893324e5496a7f14c0409d5ce76bb45908
d-weimer <dweimer19@gmail.com>
2026-04-23 15:40:16 +0900

    Added Nested Routing to About Me and About Us

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,7 +5,12 @@ import Header from "../Header/Header";
 import Dashboard from "../Dashboard/Dashboard";
 import Reviews from "../Reviews/Reviews";
 import AboutMe from "../AboutMe/AboutMe";
+import Contact from "../AboutMe/Contact";
+import Hobbies from "../AboutMe/Hobbies";
+import MyStory from "../AboutMe/MyStory";
 import AboutUs from "../AboutUs/AboutUs";
+import SiteHistory from "../AboutUs/SiteHistory";
+import SiteMission from "../AboutUs/SiteMission";
 
 function App() {
   return (
@@ -14,8 +19,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/reviews" element={<Reviews />} />
-        <Route path="/about-me" element={<AboutMe />} />
-        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/about-me" element={<AboutMe />}>
+          <Route path="contact" element={<Contact />} />
+          <Route path="hobbies" element={<Hobbies />} />
+          <Route path="my-story" element={<MyStory />} />
+        </Route>
+        <Route path="/about-us" element={<AboutUs />}>
+          <Route path="site-history" element={<SiteHistory />} />
+          <Route path="site-mission" element={<SiteMission />} />
+        </Route>
       </Routes>
     </div>
   );
